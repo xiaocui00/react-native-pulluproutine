cd8b0661333512ce2a3c1145c125f68a8bd3f41a
cuili.wang <cuili.wang@newtouch.com>
2020-12-14 17:46:49 +0800

    update

--- RNPulluproutine.podspec
+++ RNPulluproutine.podspec
@@ -6,18 +6,17 @@ Pod::Spec.new do |s|
   s.description  = <<-DESC
                   RNPulluproutine
                    DESC
-  s.homepage     = ""
+  s.homepage     = "https://github.com/xiaocui00/react-native-pulluproutine.git"
   s.license      = "MIT"
   # s.license      = { :type => "MIT", :file => "FILE_LICENSE" }
   s.author             = { "author" => "author@domain.cn" }
   s.platform     = :ios, "7.0"
-  s.source       = { :git => "https://github.com/author/RNPulluproutine.git", :tag => "master" }
-  s.source_files  = "RNPulluproutine/**/*.{h,m}"
+  s.source       = { :git => "https://github.com/xiaocui00/react-native-pulluproutine.git", :tag => "master" }
+  s.source_files  = "ios/*.{h,m}"
   s.requires_arc = true
 
-
   s.dependency "React"
-  #s.dependency "others"
+  s.dependency 'WechatOpenSDK', '~> 1.8.7.1'
 
 end
 
--- ios/RNPulluproutine.m
+++ ios/RNPulluproutine.m
@@ -1,5 +1,15 @@
 
 #import "RNPulluproutine.h"
+#import "WXApi.h"
+#import "WXApiObject.h"
+
+#define INVOKE_FAILED (@"WeChat API invoke returns false.")
+
+@interface RNPulluproutine ()
+
+@property (nonatomic, copy) RCTResponseSenderBlock callback;
+
+@end
 
 @implementation RNPulluproutine
 
@@ -8,6 +18,31 @@
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(wechatLoad:(NSString *)userName path:(NSString *)path callback:(RCTResponseSenderBlock)callback) {
+    self.callback = callback;
+//    __weak RNPulluproutine *weakself = self;
+    WXLaunchMiniProgramReq *launchMiniProgramReq = [WXLaunchMiniProgramReq object];
+    launchMiniProgramReq.userName = userName;  //拉起的小程序的username
+    launchMiniProgramReq.path = path; //拉起小程序页面的可带参路径，不填默认拉起小程序首页，对于小游戏，可以只传入query部分，来实现传参效果，如：传入 "?foo=bar"。
+    launchMiniProgramReq.miniProgramType = WXMiniProgramTypeRelease; //拉起小程序的类型
+//    WXMiniProgramTypeRelease = 0,       //**< 正式版  */
+//    WXMiniProgramTypeTest = 1,        //**< 开发版  */
+//    WXMiniProgramTypePreview = 2,         //**< 体验版  */
+    return  [WXApi sendReq:launchMiniProgramReq completion:^(BOOL success) {
+        callback(@[success ? [NSNull null] : INVOKE_FAILED]);
+    }];
+}
+
+-(void)onResp:(BaseResp *)resp
+{
+    if ([resp isKindOfClass:[WXLaunchMiniProgramResp class]])
+    {
+        NSString *string = resp.errStr;
+        // 对应JsApi navigateBackApplication中的extraData字段数据
+        NSLog(@"ios log uploadRecord finishDic %@",string);
+    }
+}
 
 @end
   
